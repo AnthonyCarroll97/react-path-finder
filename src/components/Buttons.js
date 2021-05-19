3e58450fd85398a3051032878f9f3bb0056fb03d
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2021-05-19 10:41:09 +1000

    seperated buttons into two seperate divs

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,39 +5,46 @@ export default class Buttons extends Component {
     render() {
         
         return (
-            <div className="buttons-main">
-                <button 
-                onClick={this.props.handlePress}
-                value="start"
-                className={this.props.buttonControls.setStart ? "button-active" : "hover"}
-                >
-                Set Start
-                </button>
-                
-                <button 
-                onClick={this.props.handlePress}
-                value="end"
-                className={this.props.buttonControls.setEnd ? "button-active" : "hover"}
-                >
-                Set End
-                </button>
-                <button
-                onClick={this.props.handlePress}
-                value="block"
-                className={this.props.buttonControls.setBlocks ? "button-active" : "hover"}
-                >
-                Set Block
-                </button>
-                <button
-                onClick={this.props.clearBoard}
-                className="button-large hover clear">
-                Clear
-                </button>
-                <button 
-                onClick={this.props.startSearch}
-                className="button-large hover begin"
-                >Begin</button>
-            </div>
+            <>
+                <div className="buttons-main">
+                    <button 
+                    onClick={this.props.handlePress}
+                    value="start"
+                    className={this.props.buttonControls.setStart ? "button-active" : "hover"}
+                    >
+                    Set Start
+                    </button>
+                    
+                    <button 
+                    onClick={this.props.handlePress}
+                    value="end"
+                    className={this.props.buttonControls.setEnd ? "button-active" : "hover"}
+                    >
+                    Set End
+                    </button>
+                    <button
+                    onClick={this.props.handlePress}
+                    value="block"
+                    className={this.props.buttonControls.setBlocks ? "button-active" : "hover"}
+                    >
+                    Set Block
+                    </button>
+                    
+                    
+                </div>
+                <div className="large-buttons-main">
+                    <button
+                    onClick={this.props.clearBoard}
+                    className="button-large hover clear">
+                    Clear
+                    </button>
+                    <button 
+                    onClick={this.props.startSearch}
+                    className="button-large hover begin"
+                    >Begin</button>
+
+                </div>
+            </>
         )
     }
 }
